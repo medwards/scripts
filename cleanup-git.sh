8f47cffce3a6386aea48ef3a75df9bde7a830ebb
Michael Edwards <medwards@walledcity.ca>
2012-09-20 20:32:49 +0200

    cleanup-git.sh: refactor prompt to get non-decision options more easily

--- cleanup-git.sh
+++ cleanup-git.sh
@@ -26,23 +26,26 @@ function get_reply {
 
 	echo -ne "Remove \033[1m$READ_REMOTE$BRANCH\033[0m$REMOTE_INFO?"
 	read -p " (y/n/i) "
+	REPLY=`get_first_char $REPLY`
 }
 
 
 git fetch --multiple $FORK_REMOTE $UPSTREAM_FORK
+REPLY=
 REMOTE_BRANCHES=`git branch -r | grep $FORK_REMOTE/ | grep -v master | grep -v HEAD| cut -d/ -f2`
 for BRANCH in $REMOTE_BRANCHES
 do
-	get_reply $FORK_REMOTE
-
-	while [ "`get_first_char $REPLY`" = 'i' ]
+	while [ "$REPLY" != 'y' -a "$REPLY" != 'n' ]
 	do
-		echo "$UPSTREAM..$FORK_REMOTE/$BRANCH contains:"
-		git log $UPSTREAM..$FORK_REMOTE/$BRANCH
+		if [ "$REPLY" = 'i' ]
+		then
+			echo "$UPSTREAM..$FORK_REMOTE/$BRANCH contains:"
+			git log $UPSTREAM..$FORK_REMOTE/$BRANCH
+		fi
 		get_reply $FORK_REMOTE
 	done
 
-	if [ "`get_first_char $REPLY`" = 'y' ]
+	if [ "$REPLY" = 'y' ]
 	then
 		git push origin :$BRANCH
 		git branch -D $BRANCH
@@ -54,6 +57,7 @@ then
 	exit
 fi
 
+REPLY=
 LOCAL_BRANCHES=`git branch | grep -v master | grep -v HEAD| grep -v '(no branch)' | tr -d [:blank:] | tr -d '*'`
 for BRANCH in $LOCAL_BRANCHES
 do
@@ -63,16 +67,16 @@ do
 		continue
 	fi
 
-	get_reply
-
-	while [ "`get_first_char $REPLY`" = 'i' ]
+	while [ "$REPLY" != 'y' -a "$REPLY" != 'n' ]
 	do
-		echo "$UPSTREAM..$BRANCH contains:"
-		git log $UPSTREAM..$BRANCH
-		get_reply
+		if [ "$REPLY" = 'i' ]
+		then
+			echo "$UPSTREAM..$BRANCH contains:"
+			git log $UPSTREAM..$BRANCH
+		fi
 	done
 
-	if [ "`get_first_char $REPLY`" = 'y' ]
+	if [ "$REPLY" = 'y' ]
 	then
 		git branch -D $BRANCH
 	fi
